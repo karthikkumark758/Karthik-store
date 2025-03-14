5eb3de805ad8930ce2fd9eefdb31d178cc48915c
Your Name <you@example.com>
2025-03-13 21:14:24 -0700

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,17 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperbQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReview";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Footer,
+};
